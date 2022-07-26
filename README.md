3b9e981eea68d35dace86464df07fbe66dbe5b3c
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-07-26 14:42:39 +0900

    first commit for Challenge 03: Password Generatoor

---
+++ README.md
@@ -0,0 +1,69 @@
+# 03 JavaScript: Password Generator
+
+## URL of the Deployed Application 
+[03 JavaScript: Password Generator](https://github.com/YichanYouRichard/03-Challenge-Password-Generator)
+
+## My Task
+
+The challenge #3 is to modify the starter code to create an application that enables to generate the password in given conditions.
+
+## USER Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certin criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## SCREENSHOTS
+
+The following image shows the web application's appearance and functionality:
+
+![The Password Generator application displays a red button to "Generate Password". and it has multiple prompts to select certain settings to create password.](./assets/images/03challenge-image-1.png)
+![](./assets/images/03challenge-image-2.png)
+![](./assets/images/03challenge-image-3.png)
+![](./assets/images/03challenge-image-4.png)
+![](./assets/images/03challenge-image-5.png)
+![](./assets/images/03challenge-image-6.png)
+![](./assets/images/03challenge-image-7.png)
+![](./assets/images/03challenge-image-8.png)
+
+## What I did in the coding
+
+I created the variables of character sets of lowercase, uppercase, numerics, and special characters.
+Then I created variable of final array that will be eventual array that we will going to use for generating password.
+
+In the function generatePassword, I first created final array becomes empty to reset when we already had the values in array.
+Then, I created multiple prompts for asking least number of characters, maximum number of characters, whether to include lowercase, uppercase, numeric and special characters. 
+Then, I created alert message to show error when use entered invalid numbers or characters.
+Also, I created alert message when user entered the higher minimum number than maximum's
+Then, I created return to terminate the function if the prompt get error.
+Lastly for prompt, I created if statement to show error if none of the characters have been selected. 
+
+I created password Length variable to randomize the length of character that are between minimum number and maximum number.
+I use for loop statement to generate password depends on the length of the characters that were also randomized as well.
+
+function writePassword has been written to write password in the textbox along with generate function
+
+Lastly, the generate button event has been created with addEventListener by "click" to process the code above. 
+
---
+++ script.js
@@ -0,0 +1,174 @@
+// setting variables with array of characters
+var generateBtn = document.querySelector("#generate");   
+var lowerCaseLetters = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseLetters = lowerCaseLetters.map(element => {
+    return element.toUpperCase();
+});
+var numericLetters = ["1","2","3","4","5","6","7","8","9","0"];
+var specialLetters = ["!","@","#","$","%","^","&","*","(",")","~","`",",","<",".",">","/","?",";",":","'","[","]","{","}","-","_","=","+",'"'];
+
+var finalArray = [];
+console.log(`Current list of characters that we can use is "${finalArray}".`);
+
+function generatePassword () {
+    // reset the password finalArray
+    finalArray = [];
+
+    // Ask user for Minimum number 
+    var verifyLeastNum = Number(window.prompt("Enter the minimum number of the password length. (It can't be lower than 8 characters.)","8"));
+        if ( !verifyLeastNum) {
+            console.log("Error: User has canceled the selection. Please check if user entered valid numbers.");
+            window.alert("Error: Generating password has been canceled. Please check if you entered valid numbers.")
+            return;
+        } else if (verifyLeastNum < "8") {
+            window.alert("Minimum number can't be lower than 8 characters.");
+            console.log("User has selected invalid number.");
+            return;
+        } else if (verifyLeastNum > "128") {
+            window.alert("You can not select minimum number more than 128 characters. Please try it again.")
+            console.log("User has selected invalid number.")
+            return;
+        }    else {
+            console.log("User has selected minimum number of characters.")
+        };
+
+
+        // Ask user for maximum number
+    var verifyMaxNum = Number(window.prompt("Enter the maximum number of the password length. (It can't exceed 128)", `${verifyLeastNum}`));
+        if ( !verifyMaxNum) {
+            console.log("User has canceled the selection.");
+            window.alert("Generating password has been canceled.")
+            return;
+        } else if (typeof verifyMaxNum !== "number"){
+            window.alert("This is not valid number please try it again");
+            console.log("User has selected invalid number.");
+            return;
+        } else if (verifyMaxNum < verifyLeastNum){
+            window.alert("Maximum number is lower than minimum number. Please try it again.");
+            console.log("User has selected invalid number.");
+            return;
+        } else if (verifyMaxNum > "128") {
+            window.alert("Maximum number can not be more than 128 characters. Please try it again.");
+            console.log("User has selected invalid number.");
+            return;
+        } else {
+            console.log("User has selected maximum number of characters.");
+        }   
+
+
+
+        // Ask user to use lowercase or not
+    var verifyLowercase = window.prompt("Do you want to include lowercase?","Yes / No");
+    if (!verifyLowercase) {
+        console.log("User has canceled the selection.");
+        window.alert("Generating password has been canceled.")
+            return;
+        } else if (verifyLowercase.toLowerCase() === `yes`) {
+            finalArray = finalArray.concat(lowerCaseLetters);
+            console.log("User has included the lowercase letters into the final array.");
+        } 
+        else if(verifyLowercase.toLowerCase() === `no`) {
+            console.log("User has not included the lowercase letters into the final array.");
+    } else {
+        window.alert(`This is invalid answer, please try again`);
+            console.log("User has selected invalid answer.");
+        return;
+    };
+    
+
+
+    // Ask user to use uppercase or not
+    var verifyUppercase = window.prompt("Do you want to include uppercase?","Yes / No");
+        if (!verifyUppercase) {
+            console.log("User has canceled the selection.");
+            window.alert("Generating password has been canceled.")
+            return;
+        } else if (verifyUppercase.toLowerCase() === `yes`) {
+            finalArray = finalArray.concat(upperCaseLetters);
+            console.log("User has included the uppercase letters into the final array.");
+        } 
+        else if(verifyUppercase.toLowerCase() === `no`) {
+            console.log("User has not included the uppercase letters into the final array.");
+    } else {
+        window.alert(`This is invalid answer, please try again`);
+        console.log("User has selected invalid answer.");
+        return;
+    };
+
+
+
+    // Ask user to use Numeric numbers or not
+    var verifyNumeric = window.prompt("Do you want to include numeric characters?","Yes / No");
+    if (!verifyNumeric) {
+        console.log("User has canceled the selection.");
+        window.alert("Generating password has been canceled.");
+        return;
+    } else if (verifyNumeric.toLowerCase() === `yes`) {
+        finalArray = finalArray.concat(numericLetters);
+        console.log("User has included the numeric characters into the final array.");
+    } 
+    else if(verifyNumeric.toLowerCase() === `no`) {
+    console.log("User has not included the numeric characters into the final array.");
+} else {
+    window.alert(`This is invalid answer, please try again`);
+    console.log("User has selected invalid answer.");
+    return;
+};
+
+
+
+    // ASK USER TO USE SPECIAL CHARACTERS OR NOT
+    var verifySpecial = window.prompt("Do you want to include special characters?","Yes / No");
+    if (!verifySpecial) {
+        console.log("User has canceled the selection.");
+        window.alert("Generating password has been canceled.");
+        return;
+    } else if (verifySpecial.toLowerCase() === `yes`) {
+        finalArray = finalArray.concat(specialLetters);
+        console.log("User has included the special characters into the array.");
+    } 
+    else if(verifySpecial.toLowerCase() === `no`) {
+        console.log("User has not included the special characters into the array.");
+} else {
+    window.alert(`This is invalid answer, please try again`);
+    console.log("User has selected invalid answer.");
+    return;
+};
+
+
+// Make alert message that gives error when none of the characters were selected
+
+if (finalArray.length < 1) {
+    window.alert("Please select at least one type of characters to include it in password.")
+    return;
+}
+
+
+// Randomize the password by creating length of password and randomize the index of final array
+    var passwordLength = Math.floor(Math.random() * (verifyMaxNum - verifyLeastNum)) + verifyLeastNum;
+    console.log(`The length of the password has ${passwordLength} characters.`);
+    
+    let resultPassword = "";
+    for (let i = 0; i < passwordLength; i++) {
+        resultPassword += finalArray[
+            Math.floor(Math.random() * finalArray.length)]
+    }
+    console.log(`The result of the generated password is ${resultPassword}.`)
+    return resultPassword;
+    
+};
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+        passwordText.value = password;
+        
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword
+); 
+
+
+
